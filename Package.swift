633cfe3f7bbecc21baa25fad040a702230de38ca
Daniele Valentino <dava95@libero.it>
2025-07-26 08:44:31 +0200

    fix: set minimum iOS to 13.0 for concurrency support

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "ActorDI",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ActorDI",
             targets: ["ActorDI"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ActorDI"
         ),
